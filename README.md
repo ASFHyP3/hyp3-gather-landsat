291cc17a36fb7e24dc30e79e9abb0deab50f71d6
mfangaritav <mf.angarita52@gmail.com>
2025-06-06 23:08:27 +0000

    Minimal HyP3 plugin created with the hyp3 plugin cookiecutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# HyP3 gather-landsat
+
+Plugin to get landsat scenes from an input location
---
+++ src/hyp3_gather_landsat/__init__.py
@@ -0,0 +1,10 @@
+"""Plugin to get landsat scenes from an input location"""
+
+from importlib.metadata import version
+
+
+__version__ = version(__name__)
+
+__all__ = [
+    '__version__',
+]
